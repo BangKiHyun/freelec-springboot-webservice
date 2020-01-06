34eb0d9346444cb7eebadd529d0e21aa6fecdefb
BangKiHyun <rlrlvh@naver.com>
2020-01-06 18:55:29 +0900

    게시글 등록 기능

---
+++ src/main/java/com/freelec/book/springboot/serviece/posts/PostsService.java
@@ -0,0 +1,18 @@
+package com.freelec.book.springboot.serviece.posts;
+
+import com.freelec.book.springboot.domain.posts.PostsRepository;
+import com.freelec.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto) {
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}
