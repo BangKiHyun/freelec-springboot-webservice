6d2c8d34724fac9d2bd5b4b748b6aa9eb07009a6
BangKiHyun <rlrlvh@naver.com>
2020-01-09 18:37:23 +0900

    게시글 수정 및 조회 기능(더티 체킹)

--- src/main/java/com/freelec/book/springboot/serviece/posts/PostsService.java
+++ src/main/java/com/freelec/book/springboot/serviece/posts/PostsService.java
@@ -22,7 +22,7 @@ public class PostsService {
     @Transactional
     public Long update(Long id, PostsUpdateRequestDto requestDto) {
         Posts posts = postsRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("해당 사용자가 없습니다. id=" + id));
-        posts.update(posts.getTitle(), posts.getContent());
+        posts.update(requestDto.getTitle(), requestDto.getContent());
 
         return id;
     }
