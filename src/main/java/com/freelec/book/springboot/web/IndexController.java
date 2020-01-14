03bc34bbc64e55d2f5a256e83d093f4cb6686d1e
BangKiHyun <rlrlvh@naver.com>
2020-01-14 16:51:15 +0900

    mustache -> handlebars 변경, 게시글 등록,수정 화면

--- src/main/java/com/freelec/book/springboot/web/IndexController.java
+++ src/main/java/com/freelec/book/springboot/web/IndexController.java
@@ -1,9 +1,11 @@
 package com.freelec.book.springboot.web;
 
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@AllArgsConstructor
 public class IndexController {
 
     @GetMapping("/")
